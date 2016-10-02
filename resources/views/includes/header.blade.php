7ba305d5dde930e440bc614be985e9c4577c428a
kypraw <faulkatz@gmail.com>
2016-10-02 15:04:51 +0700

    Edit Header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,12 +2,14 @@
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
+      @if(Auth::user())
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+      @endif
       <a class="navbar-brand">Survey Kepuasan Pengguna</a>
     </div>
 
